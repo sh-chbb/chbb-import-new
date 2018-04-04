f1e59fb37dd5536ccbdb80370ec14968646cabd5
James Gu <james_gu14@hotmail.com>
2018-04-04 18:18:40 +0800

    add logic

--- importCelebrity.py
+++ importCelebrity.py
@@ -2,7 +2,8 @@ import csv
 import os
 import pinyin
 import requests
-import collections
+import mysql.connector
+# import collections
 
 from models.Person import Person
 
@@ -13,7 +14,6 @@ personList = []
 
 # Step 1. Load celebs from csv file
 with open(csvFilePath, newline='') as csvFile:
-
     csvReader = csv.reader(csvFile, delimiter=',')
     for row in csvReader:
         newPerson = Person(row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
@@ -40,17 +40,45 @@ print('Total images: ', len(imageList))
 totalMatchImages = 0
 
 for person in personList:
-
     pinyinName = pinyin.get(person.name, format="strip")
+    person.pinyinName = pinyinName
 
     for imageName in imageList:
-
         if imageName.split('.')[0].split('-')[0] == pinyinName:
-
             matchImageList.append(imageName)
             person.images.append(imageName)
             totalMatchImages += 1
 
-
 # Step 4. Loop personList, upload to Cloud and save to DB
 
+
+cnx = mysql.connector.connect(user='root', password='root',
+                              host='127.0.0.1',
+                              database='chbb_facecompare_dev')
+cursor = cnx.cursor()
+
+insert_celebrity_query = 'insert into `celebrity` values (null, %s, %s, %s, %s, %s, %s, %s)'
+insert_image_query = 'insert into `celebrityImage` values (null, %s, %s, %s, %s)'
+
+for person in personList:
+
+    print(person.desc)
+
+    personDob = person.dob.replace('年', '-').replace('月', '-').replace('日', '')
+    celebrity_data = (
+        person.name, person.gender, personDob, person.job, person.nationality,
+        person.birthplace, person.desc)
+
+    cursor.execute(insert_celebrity_query, celebrity_data)
+    celebId = cursor.lastrowid
+
+    for img in person.images:
+        image_data = (celebId, img.find('child') > -1, img, person.pinyinName + '_' + str(celebId))
+        cursor.execute(insert_image_query, image_data)
+
+    print(person.name, ' inserted.')
+    cnx.commit()
+
+
+cursor.close()
+cnx.close()
--- models/Person.py
+++ models/Person.py
@@ -1,6 +1,7 @@
 class Person:
 
     name = ''
+    pinyinName = ''
     gender = ''
     dob = ''
     job = ''
@@ -12,13 +13,14 @@ class Person:
 
     def __init__(self, name, gender, dob, job, nationality, birthplace, desc, url):
         self.name = name
-        self.gender = gender,
-        self.dob = dob,
-        self.job = job,
-        self.nationality = nationality,
-        self.birthplace = birthplace,
-        self.desc = desc,
+        self.gender = gender
+        self.dob = dob
+        self.job = job
+        self.nationality = nationality
+        self.birthplace = birthplace
+        self.desc = desc
         self.url = url
 
     def print_person(self):
         print("name: ", self.name)
+
